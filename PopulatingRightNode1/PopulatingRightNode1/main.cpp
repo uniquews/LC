e6fba792a62eadfc7f40ffb221c1c29e1fe9668b
uniquews <wstjpu@gmail.com>
2014-06-11 18:46:30 -0700

    add

---
+++ PopulatingRightNode1/PopulatingRightNode1/main.cpp
@@ -0,0 +1,91 @@
+//
+//  main.cpp
+//  PopulatingRightNode1
+//
+//  Created by Shuai Wang on 6/11/14.
+//  Copyright (c) 2014 Shuai Wang. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+struct TreeLinkNode {
+      int val;
+      TreeLinkNode *left, *right, *next;
+      TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+    
+};
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(root==nullptr) return;
+        vector<TreeLinkNode *> layer;
+        queue<TreeLinkNode *> nodeQueue;
+        
+        int currentLayer = 1;
+        int nextLayer = 0;
+        
+        nodeQueue.push(root);
+        
+        while(!nodeQueue.empty()){
+            TreeLinkNode *p = nodeQueue.front();
+            nodeQueue.pop();
+            layer.push_back(p);
+            
+            if(p->left){
+                nodeQueue.push(p->left);
+                nextLayer++;
+                
+            }
+            
+            if(p->right){
+                nodeQueue.push(p->right);
+                nextLayer++;
+            }
+            
+            if(layer.size()==currentLayer){
+                for(int i = 0; i<layer.size(); i++){
+                    if(i==layer.size()-1){
+                        layer[i]->next = NULL;
+                    }else{
+                        layer[i]->next = layer[i+1];
+                    }
+                }
+                
+                currentLayer = nextLayer;
+                nextLayer = 0;
+                layer.clear();
+            }
+            
+        }
+        
+        return;
+    }
+};
+
+int main(int argc, const char * argv[])
+{
+
+    
+    TreeLinkNode *a = new TreeLinkNode(1);
+    TreeLinkNode *b = new  TreeLinkNode(2);
+    TreeLinkNode *c = new TreeLinkNode(3);
+    
+    a->left = b;
+    a->right = c;
+    
+    Solution s;
+    s.connect(a);
+    
+
+    
+    // insert code here...
+//    std::cout << "Hello, World!\n";
+    return 0;
+}
+
