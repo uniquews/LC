4171aa07fcd43b9dc108e0f420fa710cb5323bda
Shuai Wang <uniquews@users.noreply.github.com>
2014-06-20 15:18:56 -0700

    update

--- Unique_Path/Unique_Path/main.cpp
+++ Unique_Path/Unique_Path/main.cpp
@@ -16,26 +16,93 @@ using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> f;
-        f.resize(m, vector<int>(n,0));
-        for(int i=0; i<n; i++){
-            f[0][i] =1;
+//        vector<vector<int>> f;
+//        f.resize(m, vector<int>(n,0));
+//        for(int i=0; i<n; i++){
+//            f[0][i] =1;
+//        
+//        }
+//        
+//        for(int i=0; i<m; i++){
+//            f[i][0] =1;
+//        }
+//        
+//        for(int i=1; i<m; i++){
+//            for(int j =1; j<n;j++){
+//                f[i][j] = f[i-1][j] + f[i][j-1];
+//            }
+//        }
+//        
+//        return f[m-1][n-1];
+//    }
         
+
+        
+//1D array dynamic programming
+//        vector<int> f;
+//        f.resize(n, 0);
+//        f[0]=1;
+//        for(int i=0; i<m; i++){
+//            for(int j =1; j<n;j++)
+//                f[j] = f[j-1]+f[j];
+//        
+//        }
+//        
+//        return f[n-1];
+        
+        
+        
+        
+// Memoization = dfs+cache
+       vector<vector<int>> f;
+       f.resize(m+1, vector<int>(n+1,0));
+       return dfs(m,n, f);
+        
+    }
+    
+    
+    int dfs(int m, int n, vector<vector<int>> &f){
+        
+        if(m==0 && n==0) return 0;
+        if(m==1 && n==1){
+            f[m][n] = 1;
+            return f[m][n];
         }
         
-        for(int i=0; i<m; i++){
-            f[i][0] =1;
-        }
         
-        for(int i=1; i<m; i++){
-            for(int j =1; j<n;j++){
-                f[i][j] = f[i-1][j] + f[i][j-1];
+        if(m ==1){
+            if(f[m][n]>0)
+                return f[m][n];
+            else{
+                f[m][n] =dfs(1, n-1, f);
+                return f[m][n];
             }
+            
+        
         }
         
-        return f[m-1][n-1];
+        if(n == 1){
+            if(f[m][n]>0)
+                return f[m][n];
+            else{
+                f[m][n] = dfs(m-1, 1, f);
+                return f[m][n];
+            }
+        
+        }
+        
+        if(f[m][n]>0) return f[m][n];
+        else{
+            f[m][n] =dfs(m-1, n, f) + dfs(m, n-1, f);
+            return f[m][n];
+        }
+ 
+    
     }
 };
+
+
+
 
 int main(int argc, const char * argv[])
 {
