a9e99ab640c2036b663920bf8b6cda67690644a9
Shuai Wang <shuaiwang@Shuais-MacBook-Air.local>
2014-06-20 09:47:31 -0700

    add

---
+++ Surround_Regions/Surround_Regions/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Surround_Regions
+//
+//  Created by Shuai Wang on 6/20/14.
+//  Copyright (c) 2014 Shuai Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
