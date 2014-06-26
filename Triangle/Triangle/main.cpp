3f2bfd08f540223faebdec7aa8b41a1eb1782e67
Shuai Wang <uniquews@users.noreply.github.com>
2014-06-25 18:13:36 -0700

    add

---
+++ Triangle/Triangle/main.cpp
@@ -0,0 +1,64 @@
+//
+//  main.cpp
+//  Triangle
+//
+//  Created by Shuai Wang on 6/25/14.
+//  Copyright (c) 2014 Shuai Wang. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int> > &triangle) {
+        vector<vector<int>> f;
+        f.resize(triangle.size(), vector<int>(triangle.size(), 0));
+        f[0][0]= triangle[0][0];
+        return dp(triangle, f);
+        
+    }
+    
+    int dp(vector<vector<int>> &triangle, vector<vector<int>> &f){
+        int i =1;
+        for(; i<triangle.size(); i++){
+            int temp = (int) triangle[i].size();
+            for(int j = 0; j<temp; j++){
+                if(j == 0)
+                    f[i][j] = f[i-1][0]+triangle[i][j];
+                else if(j == (int)triangle[i].size()-1)
+                    f[i][j] = f[i-1][(int)triangle[i].size()-2] + triangle[i][j];
+                else
+                    f[i][j] = min(f[i-1][j], f[i-1][j-1])+triangle[i][j];
+            }
+        
+        }
+        
+        int smallest = INT_MAX;
+        int temp = (int) triangle[i-1].size();
+        for(int k = 0; k<temp; k++){
+            if(f[i-1][k] < smallest)
+                smallest = f[i-1][k];
+        
+        }
+        
+        return smallest;
+    
+    }
+};
+
+
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    vector<vector<int>> triangle = {{2}, {3, 4}, {6, 5, 7}, {4,1 ,8,3}};
+    Solution s;
+    cout <<  s.minimumTotal(triangle) << endl;
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
