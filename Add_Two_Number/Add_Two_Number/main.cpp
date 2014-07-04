0052e381e27d51fac8c88d6a9f4d65b7cfaeae38
uniquews <wstjpu@gmail.com>
2014-07-04 14:29:20 -0700

    Update main.cpp
    update

--- Add_Two_Number/Add_Two_Number/main.cpp
+++ Add_Two_Number/Add_Two_Number/main.cpp
@@ -1,28 +1,9 @@
-//
-//  main.cpp
-//  Add_Two_Number
-//
-//  Created by Shuai Wang on 7/4/14.
-//  Copyright (c) 2014 Shuai Wang. All rights reserved.
-//
-
-#include <iostream>
-
-using namespace std;
-
-struct ListNode {
-       int val;
-       ListNode *next;
-       ListNode(int x) : val(x), next(NULL) {}
-};
-
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
         ListNode *link1 = l1;
         ListNode *link2 = l2;
         ListNode *prev = l1;
-        ListNode *start = link1;
         int carry = 0;
         while(link1!=nullptr|| link2!=nullptr){
             if(link1!=nullptr && link2!=nullptr){
@@ -56,23 +37,6 @@ public:
             prev->next = addOneDigit;
         }
         
-        return start;
+        return l1;
     }
 };
-
-int main(int argc, const char * argv[])
-{
-
-    ListNode *a = new ListNode(1);
-//    ListNode *b = new ListNode (8);
-//    a->next  = b;
-    ListNode *c = new ListNode(9);
-    ListNode *d = new ListNode (9);
-    c->next = d;
-    
-    Solution s;
-    cout << s.addTwoNumbers(a, c)->val;
-    
-    return 0;
-}
-
