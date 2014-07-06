e84b8a9b04c8753400cd3ca6e5385a9a66db36db
Shuai Wang <uniquews@users.noreply.github.com>
2014-07-06 14:46:00 -0700

    add

---
+++ Remove_Duplicates_From_List/Remove_Duplicates_From_List/main.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  Remove_Duplicates_From_List
+//
+//  Created by Shuai Wang on 7/6/14.
+//  Copyright (c) 2014 Shuai Wang. All rights reserved.
+//
+
+#include <iostream>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        if(head == nullptr) return nullptr;
+        ListNode *prev = head;
+        ListNode *cur = head->next;
+        
+        while(cur){
+            if(prev->val == cur->val){
+                prev->next = cur->next;
+                delete cur;
+                cur = prev;
+            }
+            
+            prev = cur;
+            cur = cur->next;
+        }
+        
+        return head;
+    }
+};
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
