1d6ebeadff9cfd7470acbbf017c909d6cf04121e
Shuai Wang <wstjpu@gmail.com>
2014-07-07 10:37:25 -0700

    add

--- Copy_List_With_Random_Pointer/Copy_List_With_Random_Pointer/main.cpp
+++ Copy_List_With_Random_Pointer/Copy_List_With_Random_Pointer/main.cpp
@@ -8,11 +8,63 @@
 
 #include <iostream>
 
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+class Solution {
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        RandomListNode dummy(-1);
+        dummy.next = head;
+        if(head == nullptr) return nullptr;
+        
+        RandomListNode *cur = head;
+        while(cur!=nullptr){
+            RandomListNode *temp = new RandomListNode(cur->label);
+            temp->next = cur->next;
+            cur->next  = temp;
+            cur = temp->next;
+        }
+        
+        
+        cur = head;
+        while(cur != nullptr){
+            RandomListNode *temp = cur->next;
+            if(cur->random!=nullptr){
+                temp->random = cur->random->next; //!!!!!
+                
+            }
+            cur = temp->next;
+        }
+        
+        cur = head;
+        dummy.next = cur->next;
+        while(cur!=nullptr){
+            RandomListNode *temp = cur->next;
+            cur->next = temp->next;//!!!!!
+            if(temp->next!=nullptr){
+                temp->next = temp->next->next;
+            }
+            cur = cur->next;
+            
+            
+        }
+        return dummy.next;
+    }
+    
+};
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    RandomListNode *a = new RandomListNode(-1);
+    a->next = nullptr;
+    
+    Solution s;
+    s.copyRandomList(a);
+    
     return 0;
 }
 
