dc22e6779627b43546977fddff4b284d33714e5c
uniquews <wstjpu@gmail.com>
2014-07-10 15:13:05 -0700

    add

---
+++ Best_Time_To_Buy_Sale/Best_Time_To_Buy_Sale/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  Best_Time_To_Buy_Sale
+//
+//  Created by Shuai Wang on 7/10/14.
+//  Copyright (c) 2014 Shuai Wang. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        if(prices.size() < 2) return 0;
+        int curMin = 0;
+        int profit = 0;
+        curMin = prices[0];
+        
+        for(int i=1; i<prices.size(); i++){
+            profit = max(profit, prices[i] - curMin);
+            curMin = min(curMin, prices[i]);
+        }
+        
+        return profit;
+    }
+};
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
