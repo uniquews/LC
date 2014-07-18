a000bbb2a7e3f78dad65f489ce49a8dcb5302f59
uniquews <wstjpu@gmail.com>
2014-07-18 14:16:52 -0700

    add

---
+++ Unqiue_BST/Unqiue_BST/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  Unqiue_BST
+//
+//  Created by Shuai Wang on 7/18/14.
+//  Copyright (c) 2014 Shuai Wang. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> f(n+1, 0);
+        f[0] = 1;
+        f[1] = 1;
+        
+        
+        for(int i =2; i<=n; i++){
+            int temp = i-1;
+            f[i] = 0;
+            for(int j=0; j<=temp; j++){
+                f[i]+=f[j]* f[temp-j];
+            }
+        }
+        
+        return f[n];
+    }
+};
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
