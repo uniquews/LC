ae24f65f33e13bef1dd4350ec3b70009fbde1641
uniquews <wstjpu@gmail.com>
2014-08-06 09:36:16 -0700

    update pascal triangle 2

--- Pascal's Triangle II/Pascal's Triangle II/main.cpp
+++ Pascal's Triangle II/Pascal's Triangle II/main.cpp
@@ -14,46 +14,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
+        vector<int> last;
         
-        vector<int> newLine;
-        vector<int> lastLine;
-        
-        
-        // if(rowIndex==0) return newLine;
-        for(int i=0; i<=rowIndex; i++){
-            newLine.clear();
-            if(i==0){
-                newLine.push_back(1);
-                lastLine.push_back(1);
+        for(int i = 0; i <= rowIndex; i++){
+            if(i == 0){
+                last.push_back(1);
             }else{
-                
-                std::vector<int>::iterator it;
-                it=lastLine.begin();
-                lastLine.insert(it, 0);
-                // for(int j=0; j<lastLine.size();j++){
-                //      lastLine.push_back(lastLine[j]);
-                // }
-                lastLine.push_back(0);
-                
-                
-                for(int m=0; m<lastLine.size()-1; m++){
-                    int temp = lastLine[m]+lastLine[m+1];
-                    newLine.push_back(temp);
+                vector<int> :: iterator it = last.begin();
+                last.insert(it, 0);
+                last.push_back(0);
+                for(int j = 0; j <= i; j++){
+                    last[j] = last[j] + last[j + 1];
                 }
                 
-                // lastLine = newLine;
-                
-                //lastLine has its own value (0,1,0) before. If we use this way, then (1,1,0), which is wrong
-                // for(int k=0; k<newLine.size(); k++)
-                //     lastLine[k] = newLine[k];
-                
+                last.pop_back();
             }
-            
-            
         }
         
-        return lastLine;
+        
+        return last;
     }
+    
 };
 
 int main(int argc, const char * argv[])
