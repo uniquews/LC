80d167d2308d49fd771d7c20d78723d7395f6feb
uniquews <wstjpu@gmail.com>
2014-08-12 10:10:24 -0700

    update search for a range using Huang's template

--- Search_For_A_Range/Search_For_A_Range/main.cpp
+++ Search_For_A_Range/Search_For_A_Range/main.cpp
@@ -12,107 +12,63 @@
 
 using namespace std;
 
-//class Solution {
-//public:
-//    
-//    // lazy way
-//    vector<int> searchRange(int A[], int n, int target) {
-//        int low = distance(A, lower_bound(A, A+n, target));
-//        int high = distance(A, prev(upper_bound(A, A+n, target)));
-//        
-//        if(A[low]!=target){
-//            return vector<int>{-1, -1};
-//        }else{
-//            return vector<int>{low, high};
-//        }
-//    }
-//};
-
 class Solution {
 public:
-//    vector<int> searchRange(int A[], int n, int target) {
-//        auto l = distance(A, lower_bounded(A, A+n, target));
-//        auto h = distance(A, upper_bounded(l, A+n, target));
-//        
-//        if(*l!=target){
-//            return vector<int>{-1, -1};
-//        }else{
-//            return vector<int>{distance(A, l), distance(A, prev(h))};
-//        }
-//        
-//    }
-//    
-//    
-//    template <class ForwardIterator, class T>
-//    ForwardIterator lower_bounded(ForwardIterator first, ForwardIterator last, T &value){
-//        while(first < last){
-//            auto mid = next(first, distance(first, last)/2);
-//            if(value >*mid)
-//                first = ++mid;
-//            else
-//                last = mid;
-//        }
-//        
-//        return first;
-//    }
-//    
-////    template <class ForwardIterator, class T>
-////    ForwardIterator lower_bounded(ForwardIterator first, ForwardIterator last, T &value){
-////        while(first < last){
-////            auto mid = (first + last) /2;
-////            if(value >*mid)
-////                first = ++mid;
-////            else
-////                last = mid;
-////        }
-////        
-////        return first;
-////    }
-//    
-//    template <class ForwardIterator, class T>
-//    ForwardIterator upper_bounded(ForwardIterator first, ForwardIterator last, T &value){
-//        while(first < last){
-//            auto mid = next(first, distance(first, last)/2);
-//            if(value >=*mid)
-//                first = ++mid;
-//            else
-//                last = mid;
-//        }
-//        
-//        return first;
-//    }
-    
-};
-
-
-class exls{
-public:
-    template <class ForwardIterator, class T>
-    ForwardIterator lower_bounded(ForwardIterator first, ForwardIterator last, T &value){
-        while(first < last){
-            auto mid = (first + last) /2;
-            if(value >*mid)
-                first = ++mid;
-            else
-                last = mid;
+    vector<int> searchRange(int A[], int n, int target) {
+        vector<int> result;
+        
+        int first = 0;
+        int end = n - 1;
+        // search for lower bound
+        while (first + 1 < end) {
+            int mid = (first + end) / 2;
+            if(A[mid] < target) {
+                first = mid;
+            } else {
+                end = mid;
+            }
         }
         
-        return first;
+        if (A[first] == target) { // [2,2] we don't go into while loop, so we need to check A[first]
+            result.push_back(first);
+        }else if (A[end] == target) { // secondly check A[end] because when A[mid] = target, we move end = mid
+            result.push_back(end);
+        } else {
+            result.push_back(-1);
+        }
+        
+        
+        //search for upper bound
+        first = 0;
+        end = n - 1;
+        
+        while(first + 1 < end) {
+            int mid = (first + end) / 2;
+            if (A[mid] > target) {
+                end = mid;
+            } else {
+                first = mid;
+            }
+        }
+        
+        if (A[end] == target) { // same as [2,2]
+            result.push_back(end);
+        } else if (A[first] == target) {
+            result.push_back(first);
+        } else {
+            result.push_back(-1);
+        }
+        
+        return result;
     }
-
-
+    
 };
 
 
 
 int main(int argc, const char * argv[])
 {
-
-    int A[] = {1,1,1,2,3,3,4,4,4,5};
-    exls ex;
-    cout << distance (A, ex.lower_bounded(A, A+10, 1));
-    
-    
+  
     
     return 0;
 }
