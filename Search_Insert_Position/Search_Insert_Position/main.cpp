136ab70a44a23c8345d72e126ca6f1d2b23e6cc2
uniquews <wstjpu@gmail.com>
2014-08-12 12:42:40 -0700

    add

--- Search_Insert_Position/Search_Insert_Position/main.cpp
+++ Search_Insert_Position/Search_Insert_Position/main.cpp
@@ -10,39 +10,78 @@
 
 using namespace std;
 
+// My answer:
+
+//class Solution {
+//public:
+//    int searchInsert(int A[], int n, int target) {
+//        if (n == 0){
+//            return 0;
+//        }
+//        
+//        int first = 0;
+//        int end = n - 1;
+//        int mid = (first + end) / 2;
+//        
+//        while(first <= end){
+//            mid = (first + end) / 2;
+//            if (A[mid] == target){
+//                return mid;
+//            }else if (A[mid] < target){
+//                first = mid + 1;
+//            }else{
+//                end = mid - 1;
+//            }
+//        }
+//        
+//        if (A[mid] < target){
+//            return mid + 1;
+//        }
+//        
+//        if (A[mid] > target){
+//            return mid;
+//        }
+//        
+//        return -1;
+//    }
+//    
+//};
+
+
+// Using template
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
-        if (n == 0){
-            return 0;
+        if (n == 0) {
+            return -1;
         }
         
         int first = 0;
         int end = n - 1;
-        int mid = (first + end) / 2;
-        
-        while(first <= end){
-            mid = (first + end) / 2;
-            if (A[mid] == target){
+        while (first + 1 < end) {
+            int mid = first + (end - first) / 2;
+            if (A[mid] == target) {
                 return mid;
-            }else if (A[mid] < target){
-                first = mid + 1;
-            }else{
-                end = mid - 1;
+            } else if (A[mid] > target) {
+                end = mid;
+            } else {
+                first = mid;
             }
         }
         
-        if (A[mid] < target){
-            return mid + 1;
+        if (A[first] == target) {
+            return first;
+        } else if (A[end] == target) {
+            return end;
+        } else if (A[end] < target) {
+            return end + 1;
+        } else if (A[first] > target) {
+            return first + 1;
+        } else {
+            return first + 1;
         }
         
-        if (A[mid] > target){
-            return mid;
-        }
-        
-        return -1;
     }
-    
 };
 
 int main(int argc, const char * argv[])
