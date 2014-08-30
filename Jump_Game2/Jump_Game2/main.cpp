533bb426a48b1ca174843cb4e07ed50acc95747b
uniquews <wstjpu@gmail.com>
2014-08-29 20:29:58 -0700

    update

--- Jump_Game2/Jump_Game2/main.cpp
+++ Jump_Game2/Jump_Game2/main.cpp
@@ -11,56 +11,27 @@
 
 using namespace std;
 
-//class Solution {
-//public:
-//    int jump(int A[], int n) {
-//        int left = 0;
-//        int right = 0;
-//        int step = 0;
-//        if(n == 1) return 0; // !!!!
-//        while(left <= right){
-//            step++;
-//            int oldRight = right;
-//            for(int i = left; i<= oldRight; i++){
-//                int temp = i + A[i];
-//                if(temp >= n-1) return step;
-//                if(temp > right) right = temp; //!!!
-//                
-//            }
-//            
-//            left = oldRight+1;
-//        }
-//        
-//        return 0;
-//    }
-//    
-//};
 
 class Solution {
 public:
     int jump(int A[], int n) {
         if (n <= 1) {
-            return n;
+            return 0;
         }
         vector<int> table(n, INT_MAX);
         table[0] = 0;
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (table[j] + A[j] >= i) {
+                if (table[j] != INT_MAX && j + A[j] >= i) {
                     table[i] = min(table[i], table[j] + 1);
-                } else {
-                    continue;
+                    break;
                 }
-                
             }
         }
         
-        if (table[n - 1] == INT_MAX) {
-            return -1;
-        } else {
-            return table[n - 1];
-        }
+        return table[n - 1];
     }
+    
 };
 
 int main(int argc, const char * argv[])
