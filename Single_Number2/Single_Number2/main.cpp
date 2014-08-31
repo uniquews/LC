d8ebbc8a85727de98fe72a9b1b76eb71696350ab
uniquews <wstjpu@gmail.com>
2014-08-31 10:39:29 -0700

    add single number

--- Single_Number2/Single_Number2/main.cpp
+++ Single_Number2/Single_Number2/main.cpp
@@ -2,36 +2,24 @@
 //  main.cpp
 //  Single_Number2
 //
-//  Created by Shuai Wang on 7/3/14.
+//  Created by Shuai Wang on 8/31/14.
 //  Copyright (c) 2014 Shuai Wang. All rights reserved.
 //
 
 #include <iostream>
-#include <unordered_map>
-
-using namespace std;
 
 class Solution {
 public:
     int singleNumber(int A[], int n) {
-        unordered_map<int, int> tempMap;
+        
         int result = 0;
-
-        for(size_t i = 0; i<n; i++){
-            unordered_map<int, int> ::iterator it = tempMap.find(A[i]);
-            if(it!=tempMap.end()){
-                it->second++;
-            }else{
-                tempMap[A[i]] = 1;
+        for (int i = 0; i < 32; i++) {
+            int count  = 0;
+            for (int j = 0; j < n; j++) {
+                count += ((A[j] >> i) & 1);
             }
-        }
-        
-        
-        unordered_map<int, int> :: iterator it2 = tempMap.begin();
-        for(; it2!=tempMap.end(); it2++){
-            if(it2->second == 1){
-                result = it2->first;
-            }
+            
+            result |= (count % 3) << i;
         }
         
         return result;
