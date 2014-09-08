fb7d6a45001310193fa7046ccf390a71e095de65
uniquews <wstjpu@gmail.com>
2014-09-08 10:02:55 -0700

    update

--- Word_Search/Word_Search/main.cpp
+++ Word_Search/Word_Search/main.cpp
@@ -11,58 +11,119 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    bool exist(vector<vector<char> > &board, string word) {
+//        string firstLetter = word.substr(0,1);
+//        
+//        int row = board.size();
+//        int column = board[0].size();
+//        
+//        vector<vector<bool>> visited (row, vector<bool> (column, false));
+//        
+//        for(int i=0; i<row; i++){
+//            for(int j=0; j<column; j++){
+//                if(board[i][j] == *firstLetter.c_str()){
+//                    if(dfs(board, word, 0, i, j, visited))
+//                        return true;
+//                }
+//            }
+//        }
+//        
+//        return false;
+//    }
+//    
+//    
+//    bool dfs(vector<vector<char>> &board, string word, int position, int i, int j, vector<vector<bool>> &visited){
+//        int row = board.size()-1;
+//        int column = board[0].size()-1;
+//        if(i <0 || j<0 || i>row || j> column)
+//            return false;
+//        
+//        if(position == word.size()-1){
+//            if(visited[i][j] == false && word[position] == board[i][j]){
+//                visited[i][j];
+//                return true;
+//            }else
+//                return false;
+//        }else{
+//            if(visited[i][j] == true)
+//                return false;
+//            else{
+//                if(word[position] == board[i][j]){
+//                    visited[i][j] = true;
+//                    
+//                    if(dfs(board, word, position+1, i-1, j, visited) || dfs(board, word, position+1, i+1, j, visited) ||
+//                       dfs(board, word, position+1, i, j-1, visited) || dfs(board, word, position+1, i, j+1, visited))
+//                        return true;
+//                }
+//                
+//                visited[i][j] = false;
+//                return false;
+//            }
+//        }
+//    }
+//};
+
+
 class Solution {
 public:
     bool exist(vector<vector<char> > &board, string word) {
-        string firstLetter = word.substr(0,1);
-        
         int row = board.size();
         int column = board[0].size();
         
-        vector<vector<bool>> visited (row, vector<bool> (column, false));
+        vector<vector<bool>> visited;
         
-        for(int i=0; i<row; i++){
-            for(int j=0; j<column; j++){
-                if(board[i][j] == *firstLetter.c_str()){
-                    if(dfs(board, word, 0, i, j, visited))
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(board, word, i, j, 0, visited)) {
                         return true;
+                    }
                 }
+                
             }
         }
         
         return false;
+        
     }
     
     
-    bool dfs(vector<vector<char>> &board, string word, int position, int i, int j, vector<vector<bool>> &visited){
-        int row = board.size()-1;
-        int column = board[0].size()-1;
-        if(i <0 || j<0 || i>row || j> column)
-            return false;
+    bool dfs(vector<vector<char>> board, string word, int i, int j, int index, vector<vector<bool>> &visited) {
+        int row = board.size();
+        int column = board[0].size();
         
-        if(position == word.size()-1){
-            if(visited[i][j] == false && word[position] == board[i][j]){
-                visited[i][j];
+        if (i < 0 || j < 0 || i >= row || j >= column) {
+            return false;
+        }
+        
+        if (index == word.size() - 1) {
+            if (word[index] == board[i][j] && visited[i][j] == false) {
                 return true;
-            }else
+            } else {
                 return false;
-        }else{
-            if(visited[i][j] == true)
-                return false;
-            else{
-                if(word[position] == board[i][j]){
-                    visited[i][j] = true;
-                    
-                    if(dfs(board, word, position+1, i-1, j, visited) || dfs(board, word, position+1, i+1, j, visited) ||
-                       dfs(board, word, position+1, i, j-1, visited) || dfs(board, word, position+1, i, j+1, visited))
-                        return true;
+            }
+        } else {
+            if (word[index] == board[i][j] && visited[i][j] == false) {
+                visited[i][j] = true;
+                if (dfs(board, word, i + 1, j, index + 1, visited)
+                    || dfs(board, word, i - 1, j, index + 1, visited)
+                    || dfs(board, word, i, j + 1, index + 1, visited)
+                    || dfs(board, word, i, j - 1, index + 1, visited)) {
+                    return true;
+                } else {
+                    visited[i][j] = false;
+                    return false;
                 }
                 
-                visited[i][j] = false;
+            } else {
                 return false;
             }
         }
     }
+    
+    
 };
 int main(int argc, const char * argv[])
 {
@@ -75,9 +136,12 @@ int main(int argc, const char * argv[])
     
 //    vector<vector<char>> board = {{'A', 'B', 'C', 'E'}, {'S','F','C','S'}, {'A','D','E','E'}};
     
-    vector<vector<char>> board = {{'C','A','A'},{'A','A','A'}, {'B','C','D'}};
-    string word = "AAB";
+//    vector<vector<char>> board = {{'C','A','A'},{'A','A','A'}, {'B','C','D'}};
+    vector<vector<char>> board = {{'a'}};
+    string word = "a";
     cout << s.exist(board, word) << endl;
+    
+
     
     std::cout << "Hello, World!\n";
     return 0;
