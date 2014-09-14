56d2b96a291e15e97a3e479aa6008da45fedd795
uniquews <wstjpu@gmail.com>
2014-09-13 18:28:11 -0700

    update

--- Maximum_Subarray/Maximum_Subarray/main.cpp
+++ Maximum_Subarray/Maximum_Subarray/main.cpp
@@ -11,39 +11,37 @@
 
 using namespace std;
 
+
 class Solution {
 public:
     int maxSubArray(int A[], int n) {
-        // i ... j sum is equal to sum[j] - sum[i - 1]
-        
-        vector<int> sum(n, 0);
+        int prev = 0;
+        vector<int> sum(n);
         for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                sum[0] = A[0];
-            } else {
-                sum[i] = sum[i - 1] + A[i];
-            }
+            prev = prev + A[i];
+            sum[i] = prev;
         }
         
-        int result = sum[0]; // not like stock, you have to pick at least one as result
-        int valley = 0; // sum[j] - sum [0] is 1 ....j. In order to get sum[0], valley shoule be 0 at the beginning
+        int _min = 0;
+        int result = INT_MIN;
         for (int i = 0; i < sum.size(); i++) {
-            result = max(result, sum[i] - valley);
-            valley = min(valley, sum[i]);
+            result = max(result, sum[i] - _min);
+            
+            _min = min(_min, sum[i]);
         }
         
         return result;
     }
-    
 };
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    int A[9] = {-2 ,1, -3, 4, -1, 2, 1, -5, 4};
+//    int A[9] = {-2 ,1, -3, 4, -1, 2, 1, -5, 4};
+    int A[] = {-2, 1};
     Solution s;
-    cout << s.maxSubArray(A, 9) << endl;
+    cout << s.maxSubArray(A, 2) << endl;
     
     return 0;
 }
