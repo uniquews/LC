bea2262018333e9121c10a996deaaf61753b8690
uniquews <wstjpu@gmail.com>
2014-09-13 22:28:05 -0700

    update

--- Interleaving_String/Interleaving_String/main.cpp
+++ Interleaving_String/Interleaving_String/main.cpp
@@ -15,38 +15,35 @@ using namespace std;
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        if(s1.length() == 0 && s2.length()==0 && s3.length() == 0)
-            return true;
-
         
-        if(s3.length() != s1.length() + s2.length())
+        if (s1.size() == 0 && s2.size() == 0 && s3.size() == 0) {
+            return true;
+        }
+        
+        if (s3.length() != s1.length() + s2.length())
             return false;
         
-        vector<vector<bool>> f (s1.length()+1, vector<bool> (s2.length()+1, false));
-        
+        vector<vector<bool>> f(s1.size() + 1, vector<bool> (s2.size() + 1));
         f[0][0] = true;
         
-        for(int i=1; i<=(int)s1.length(); i++){
-            f[i][0] = s1[i-1] == s3[i-1] && f[i-1][0];
-        
+        for (int i = 1; i <= s2.size(); i++) {
+            f[0][i] = (s2[i - 1] == s3[i - 1]) && f[0][i - 1];
         }
         
-        for(int i=1; i<=(int)s2.length(); i++){
-            f[0][i] = s2[i-1] == s3[i-1] && f[0][i-1];
+        for (int i = 1; i <= s1.size(); i++) {
+            f[i][0] = (s1[i - 1] == s3[i - 1]) && f[i - 1][0];
         }
         
-        for(int i=1; i<=(int)s1.length(); i++){
-            for(int j = 1; j<=(int)s2.length(); j++){
-                f[i][j] = (s1[i-1] == s3[i+j-1] && f[i-1][j]) ||
-                (s2[j-1] == s3[i+j-1] && f[i][j-1]);
-            
+        
+        for (int i = 1; i <= s1.size(); i++) {
+            for (int j = 1; j <= s2.size(); j++) {
+                f[i][j] = ((s1[i - 1] == s3[i + j - 1]) && f[i - 1][j]) || ((s2[j - 1] == s3[i + j - 1]) && f[i][j - 1]);
             }
-            
         }
         
-        return f[s1.length()][s2.length()];
-        
+        return f[s1.size()][s2.size()];
     }
+    
 };
 
 int main(int , const char * argv[])
--- Print_BST_Key_In_Given_Range/Print_BST_Key_In_Given_Range/main.cpp
+++ Print_BST_Key_In_Given_Range/Print_BST_Key_In_Given_Range/main.cpp
@@ -21,68 +21,102 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution {
-
-public:
-    
-    void printKey (TreeNode *root, vector<int> range) {
-        
-        if (root == nullptr) {
-            return ;
-        }
-        
-        printKeyInSubTree(root, range);
-     
-//        printKeyInSubTree(root, range, result);
-//        sort(result.begin(), result.end());
-//        adda(result);
-        
-  
-    };
-    
-    
-    void printKeyInSubTree(TreeNode *node, vector<int> range) {
-        if (node == nullptr) {
-            return;
-        }
-        
-//        if (node->val > range[1]) {
-//            printKeyInSubTree(node->left, range, result);
+//class Solution {
+//
+//public:
+//    
+//    void printKey (TreeNode *root, vector<int> range) {
+//        
+//        if (root == nullptr) {
+//            return ;
+//        }
+//        
+//        printKeyInSubTree(root, range);
+//     
+////        printKeyInSubTree(root, range, result);
+////        sort(result.begin(), result.end());
+////        adda(result);
+//        
+//  
+//    };
+//    
+//    
+//    void printKeyInSubTree(TreeNode *node, vector<int> range) {
+//        if (node == nullptr) {
 //            return;
+//        }
+//        
+////        if (node->val > range[1]) {
+////            printKeyInSubTree(node->left, range, result);
+////            return;
+////        }
+////        
+////        if (node->val >= range[0] && node->val <= range[1]) {
+////            result.push_back(node->val);
+////            cout<< node->val << endl;
+////            vector<int> newRange1 = {range[0], node->val};
+////            vector<int> newRange2 = {node->val, range[1]};
+////            
+////            printKeyInSubTree(node->left, newRange1, result);
+////            printKeyInSubTree(node->right, newRange2, result);
+////            return;
+////        }
+////        
+////        if (node->val < range[0]) {
+////            printKeyInSubTree(node->right, range, result);
+////            return;
+////        }
+//        
+//        if (node->val > range[0]) {
+//            printKeyInSubTree(node->left, range);
 //        }
 //        
 //        if (node->val >= range[0] && node->val <= range[1]) {
-//            result.push_back(node->val);
-//            cout<< node->val << endl;
-//            vector<int> newRange1 = {range[0], node->val};
-//            vector<int> newRange2 = {node->val, range[1]};
-//            
-//            printKeyInSubTree(node->left, newRange1, result);
-//            printKeyInSubTree(node->right, newRange2, result);
-//            return;
+//            cout << node->val<<" ";
 //        }
 //        
-//        if (node->val < range[0]) {
-//            printKeyInSubTree(node->right, range, result);
-//            return;
+//        if (node->val < range[1]) {
+//            printKeyInSubTree(node->right, range);
 //        }
-        
-        if (node->val > range[0]) {
-            printKeyInSubTree(node->left, range);
+//        
+//        return;
+//    }
+//    
+//
+//};
+
+class Solution {
+public:
+    int bstRangeSum(TreeNode *root, int start, int end) {
+        vector<int> result;
+        bstRange(root, start, end, result);
+        int sum = 0;
+        for (int i = 0; i < result.size(); i++) {
+            sum += result[i];
         }
         
-        if (node->val >= range[0] && node->val <= range[1]) {
-            cout << node->val<<" ";
+        return sum;
+    }
+    
+    void bstRange(TreeNode *root, int start, int end, vector<int> &result) {
+        if (root == nullptr) {
+            return;
         }
         
-        if (node->val < range[1]) {
-            printKeyInSubTree(node->right, range);
+        if (root -> val < start) {
+            bstRange(root -> right, start, end, result);
+        } else if (root -> val > end) {
+            bstRange(root -> left, start, end, result);
+        } else {
+            result.push_back(root -> val);
+            bstRange(root -> left, start, end, result);
+            bstRange(root -> right, start, end, result);
         }
         
         return;
+        
     }
     
-
 };
 
 int main(int argc, const char * argv[])
@@ -102,7 +136,8 @@ int main(int argc, const char * argv[])
     vector<int> range = {10, 22};
     
     Solution su;
-    su.printKey(a, range);
+//    su.printKey(a, range);
+    cout << su.bstRangeSum(a, 10, 20);
 
     
 //    vector<int> :: iterator it = result.begin();
