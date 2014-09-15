91f64070f3b6b207679effd480d5abbc45e93701
uniquews <wstjpu@gmail.com>
2014-09-15 01:01:24 -0700

    update

--- Largest_Rectangle_In_Histogram/Largest_Rectangle_In_Histogram/main.cpp
+++ Largest_Rectangle_In_Histogram/Largest_Rectangle_In_Histogram/main.cpp
@@ -12,6 +12,40 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int largestRectangleArea(vector<int> &height) {
+//        if (height.size() == 0) {
+//            return 0;
+//        }
+//        
+//        stack<int> stk;
+//        int result = 0;
+//        for (int i = 0; i <= height.size(); i++) {
+//            int curt = i == height.size() ? -1 : height[i];
+//            while (stk.size() != 0 && curt < height[stk.top()]) {
+//
+//                int h = height[stk.top()];
+//                stk.pop();
+//                
+//                int w = 0;
+//                if (stk.size() == 0) {
+//                    w = i;
+//                } else {
+//                    w = i - stk.top() - 1;
+//                }
+//                result = max(result, h * w);
+//                
+//            }
+//            
+//            stk.push(i);
+//        }
+//        
+//        return result;
+//    }
+//    
+//};
+
 class Solution {
 public:
     int largestRectangleArea(vector<int> &height) {
@@ -19,23 +53,22 @@ public:
             return 0;
         }
         
+        
         stack<int> stk;
         int result = 0;
         for (int i = 0; i <= height.size(); i++) {
-            int curt = i == height.size() ? -1 : height[i];
-            while (stk.size() != 0 && curt < height[stk.top()]) {
-
+            int cur = i == height.size() ? -1 : height[i];
+            while (stk.size() != 0 && cur < stk.top()) {
                 int h = height[stk.top()];
                 stk.pop();
-                
                 int w = 0;
                 if (stk.size() == 0) {
                     w = i;
                 } else {
                     w = i - stk.top() - 1;
                 }
-                result = max(result, h * w);
                 
+                result = max(result, h * w);
             }
             
             stk.push(i);
@@ -43,7 +76,6 @@ public:
         
         return result;
     }
-    
 };
 
 
@@ -52,7 +84,8 @@ public:
 int main(int argc, const char * argv[])
 {
 
-    vector<int> h = {8, 4};
+//    vector<int> h = {8, 4};
+    vector<int> h = {0};
     Solution su;
     su.largestRectangleArea(h);
     return 0;
--- Word_Break/Word_Break/main.cpp
+++ Word_Break/Word_Break/main.cpp
@@ -2,71 +2,44 @@
 //  main.cpp
 //  Word_Break
 //
-//  Created by Shuai Wang on 6/29/14.
+//  Created by Shuai Wang on 9/15/14.
 //  Copyright (c) 2014 Shuai Wang. All rights reserved.
 //
 
 #include <iostream>
 #include <string>
-#include <unordered_set>
 #include <vector>
-
-
-// even more makeing sence version
+#include <unordered_set>
 
 using namespace std;
+
 
 class Solution {
 public:
     bool wordBreak(string s, unordered_set<string> &dict) {
-        vector<vector<bool>> f(s.length(), vector<bool> (s.length(), false));
-        
-        int totalLength = s.length();
-        
-        for(int length = 1; length <= totalLength; length++){
-            for(int start = 0; start <= totalLength - length; start++){
-                int endIndex = start + length - 1;
-                string tempStr = s.substr(start, length);
-                for(int k = 1; k <= length; k++){
-                    string first = s.substr(start, k);
-                    if(start + k > endIndex){
-                        if((f[start][start + k - 1] || find(dict.begin(), dict.end(), first) != dict.end())){
-                            f[start][endIndex] = true;
-                        }
-                    }else{
-                        string second = s.substr(start + k, length);
-                        if((f[start][start + k -1] || find(dict.begin(), dict.end(), first) != dict.end()) &&
-                           (f[start + k][start + length - 1] || find(dict.begin(), dict.end(), second)!= dict.end())){
-                            f[start][endIndex] = true;
-                        }
-                        
-                    }
+        vector<bool> f(s.length() + 1, false);
+        f[0] = true;  // !!!!!!!!!!!!!!!
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (f[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    f[i] = true;
+                    break;
                 }
             }
         }
         
-        return f[0][totalLength-1];
+        return f[s.length()];
+        
     }
+    
+    
 };
-
 
 int main(int argc, const char * argv[])
 {
 
-    string s = "leetcode";
-    string trya = "";
-    string ltt = "leet";
-    unordered_set<string> _set ({"leet", "code"});
-    
-//    if(_set.find(ltt)!=_set.end())
-//        cout << "vuhjbijbkj";
-//    else
-//        cout << "hh";
-    
-    
-    Solution ss;
-    cout << ss.wordBreak(s, _set)<< endl;
-    
+    // insert code here...
+    std::cout << "Hello, World!\n";
     return 0;
 }
 
