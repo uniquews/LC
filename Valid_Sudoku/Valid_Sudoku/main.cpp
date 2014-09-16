665e85da6d32f65341cc65a5d7f81105133344cb
uniquews <wstjpu@gmail.com>
2014-09-15 22:48:07 -0700

    update

--- Valid_Sudoku/Valid_Sudoku/main.cpp
+++ Valid_Sudoku/Valid_Sudoku/main.cpp
@@ -15,49 +15,64 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char> > &board) {
-        
-        //   bool used[9];
-        vector<bool> used(9, false);
-        
-        for (int i = 0; i < 9; ++i) {
-            fill(used.begin(), used.begin()+9, false);
-            for(int j=0;j<9;++j)// 检查行
-                if (!check(board[i][j], used))
-                    return false;
-            
-            fill(used.begin(), used.begin()+9, false);
-            for(int j=0;j<9;++j)// 检查列
-                if (!check(board[j][i], used))
-                    return false;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    if (isValid(board, i, j) == false) {
+                        return false;
+                    }
+                }
+            }
         }
         
-        for(int r=0;r<3;++r)// 检查 9 个子格子
-            for (int c = 0; c < 3; ++c) {
-                fill(used.begin(), used.begin()+9, false);
-                for (int i = r * 3; i < r * 3 + 3; ++i)
-                    for (int j = c * 3; j < c * 3 + 3; ++j)
-                        if (!check(board[i][j], used))
-                            return false;
+        return true;
+    }
+    
+    bool isValid(vector<vector<char>> &board, int row, int column) {
+        for (int i = 0; i < 9; i++) {
+            if (column != i && board[row][column] == board[row][i]) {
+                return false;
             }
+        }
+        
+        
+        for (int i = 0; i < 9; i++) {
+            if (row != i && board[row][column] == board[i][column]) {
+                return false;
+            }
+        }
+        
+        int rowMod = row / 3;
+        int colMod = column / 3;
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (row != rowMod * 3 + i && column != colMod * 3 + j && board[row][column] == board[rowMod * 3 + i][colMod * 3 + j]) {
+                    return false;
+                }
+            }
+        }
+        
         return true;
     }
-    
-    bool check(char ch, vector<bool> &used) {
-        if (ch == '.') return true;
-        if (used[ch - '1']) return false;
-        used[ch - '1'] = true;
-        return true;
-    }
-    
-    
-    
 };
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<vector<char>> board = {{'.','.','.','.','.','.','.','.','2'},
+                                {'.','.','.','.','.','.','6','.','.'},
+                                {'.','.','1','4','.','8','.','.'},
+                                {'.','.','.','.','.','.','.','.','.'},
+                                {'.','.','.','.','.','.','.','.','.'},
+                                {'.','.','.','.','3','.','.','.','.'},
+                                {'5','.','8','6','.','.','.','.','.'},
+                                {'.','9','.','.','.','.','4','.','.'},
+                                {'.','.','.','.','5','.','.','.','.'}
+        
+    };
+    
+    Solution su;
+    cout << su.isValidSudoku(board) << endl;
     return 0;
 }
 
