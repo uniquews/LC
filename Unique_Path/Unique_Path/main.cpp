0026b6aca3396c2ce957cb361379ff2bf808736f
uniquews <wstjpu@gmail.com>
2014-09-15 20:01:24 -0700

    update

--- Unique_Path/Unique_Path/main.cpp
+++ Unique_Path/Unique_Path/main.cpp
@@ -13,9 +13,9 @@ using namespace std;
 
 
 //2D array dynamic programming
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
+//class Solution {
+//public:
+//    int uniquePaths(int m, int n) {
 //        vector<vector<int>> f;
 //        f.resize(m, vector<int>(n,0));
 //        for(int i=0; i<n; i++){
@@ -54,53 +54,77 @@ public:
         
         
 // Memoization = dfs+cache
-       vector<vector<int>> f;
-       f.resize(m+1, vector<int>(n+1,0));
-       return dfs(m,n, f);
-        
-    }
-    
-    
-    int dfs(int m, int n, vector<vector<int>> &f){
-        
-        if(m==0 && n==0) return 0;
-        if(m==1 && n==1){
-            f[m][n] = 1;
-            return f[m][n];
-        }
-        
-        
-        if(m ==1){
-            if(f[m][n]>0)
-                return f[m][n];
-            else{
-                f[m][n] =dfs(1, n-1, f);
-                return f[m][n];
-            }
-            
-        
-        }
-        
-        if(n == 1){
-            if(f[m][n]>0)
-                return f[m][n];
-            else{
-                f[m][n] = dfs(m-1, 1, f);
-                return f[m][n];
-            }
-        
-        }
-        
-        if(f[m][n]>0) return f[m][n];
-        else{
-            f[m][n] =dfs(m-1, n, f) + dfs(m, n-1, f);
-            return f[m][n];
-        }
- 
-    
-    }
-};
+//       vector<vector<int>> f;
+//       f.resize(m+1, vector<int>(n+1,0));
+//       return dfs(m,n, f);
+//        
+//    }
+//    
+//    
+//    int dfs(int m, int n, vector<vector<int>> &f){
+//        
+//        if(m==0 && n==0) return 0;
+//        if(m==1 && n==1){
+//            f[m][n] = 1;
+//            return f[m][n];
+//        }
+//        
+//        
+//        if(m ==1){
+//            if(f[m][n]>0)
+//                return f[m][n];
+//            else{
+//                f[m][n] =dfs(1, n-1, f);
+//                return f[m][n];
+//            }
+//            
+//        
+//        }
+//        
+//        if(n == 1){
+//            if(f[m][n]>0)
+//                return f[m][n];
+//            else{
+//                f[m][n] = dfs(m-1, 1, f);
+//                return f[m][n];
+//            }
+//        
+//        }
+//        
+//        if(f[m][n]>0) return f[m][n];
+//        else{
+//            f[m][n] =dfs(m-1, n, f) + dfs(m, n-1, f);
+//            return f[m][n];
+//        }
+// 
+//    
+//    }
+//};
 
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> f(m, vector<int> (n, 0));
+        f[0][0] = 1;
+        for (int column = 1; column < n; column++) {
+            f[0][column] = f[0][column - 1];
+        }
+        
+        for (int row = 1; row < m; row++) {
+            f[row][0] = f[row - 1][0];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        
+        return f[m - 1][n - 1];
+    }
+    
+};
 
 
 
--- Unique_Path2/Unique_Path2/main.cpp
+++ Unique_Path2/Unique_Path2/main.cpp
@@ -11,71 +11,59 @@
 
 using namespace std;
 
+
+
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        int row = obstacleGrid.size();
+        int column = obstacleGrid[0].size();
         
-        int m = (int)obstacleGrid.size();
-        int n =(int)obstacleGrid[0].size();
+        vector<vector<int>> f (row, vector<int> (column, 0));
+        if (obstacleGrid[0][0] == 1) {
+            f[0][0] = 0;
+            return 0;
+        } else {
+            f[0][0] = 1;
+        }
         
-        for(int i=0; i<m; i++){
-            for(int j =0; j<n; j++){
-                if(obstacleGrid[i][j]==1)
-                    obstacleGrid[i][j] = INT_MIN;
+        for (int i = 1; i < obstacleGrid[0].size(); i++) {
+            if (obstacleGrid[0][i] == 0) {
+                f[0][i] = f[0][i - 1];
+            } else {
+                f[0][i] = 0;
             }
         }
         
-        if(obstacleGrid[0][0] == 0){
-            obstacleGrid[0][0] = 1;
+        for (int i = 1; i < obstacleGrid.size(); i++) {
+            if (obstacleGrid[i][0] == 0) {
+                f[i][0] = f[i - 1][0];
+            } else {
+                f[i][0] = 0;
+            }
         }
         
-
-            for(int i=1; i<n; i++){
-                if(obstacleGrid[0][i]!=INT_MIN && obstacleGrid[0][i-1]!=INT_MIN)
-                    obstacleGrid[0][i] = obstacleGrid[0][i-1];
-               }
         
-            for(int i=1; i<m; i++){
-                if(obstacleGrid[i][0]!=INT_MIN && obstacleGrid[i-1][0]!=INT_MIN)
-                    obstacleGrid[i][0] =obstacleGrid[i-1][0];
-            }
-        
-            for(int i=1; i<m; i++){
-                for(int j =1; j<n;j++){
-                    if(obstacleGrid[i][j]!=INT_MIN){
-                        if(obstacleGrid[i-1][j]!=INT_MIN && obstacleGrid[i][j-1]!=INT_MIN)
-                            obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
-                        else if(obstacleGrid[i-1][j]!=INT_MIN && obstacleGrid[i][j-1]==INT_MIN){
-                            obstacleGrid[i][j] = obstacleGrid[i-1][j];
-                        }else if(obstacleGrid[i-1][j]==INT_MIN && obstacleGrid[i][j-1]!=INT_MIN){
-                            obstacleGrid[i][j] = obstacleGrid[i][j-1];
-                        }
-                    }
-                    
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    f[i][j] = f[i - 1][j] + f[i][j - 1];
+                } else {
+                    f[i][j] = 0;
                 }
             }
-        
-        for(int i=0 ;i<m; i++){
-            for(int j=0; j<n; j++){
-                if(obstacleGrid[i][j] == INT_MIN)
-                    obstacleGrid[i][j] = 0;
-                
-            
-            }
         }
         
-        
-               return obstacleGrid[m-1][n-1];
-        
+        return f[row - 1][column - 1];
     }
 };
-
 
 
 int main(int argc, const char * argv[])
 {
 
-    vector<vector<int>> obstacleGrid = {{0,1,0,0,0}, {1,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
+//    vector<vector<int>> obstacleGrid = {{0,1,0,0,0}, {1,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
+    vector<vector<int>> obstacleGrid = {{0,0}};
     Solution s;
     cout <<  s.uniquePathsWithObstacles(obstacleGrid) << endl;
     return 0;
