0aee83af37f9f1d3ac6e2aa22b849191ec707003
Shuai Wang <wstjpu@gmail.com>
2014-09-17 15:26:38 -0700

    Update main.cpp

--- String_To_Integer/String_To_Integer/main.cpp
+++ String_To_Integer/String_To_Integer/main.cpp
@@ -15,40 +15,48 @@ class Solution {
 public:
     int atoi(const char *str) {
         int i = 0;
-        int length = strlen(str);
         int sign = 1;
+        int length = strlen(str);
         int result = 0;
         
-        while(str[i]==' ') {i++;}
-        
-        if(str[i] == '+'){ // e.g.: +-2
-            if(str[i+1] == '+' || str[i+1]=='-')
-                return 0;
-            else
-                i++;
-            
+        while (str[i] == ' ') {
+            i++;
         }
-        if(str[i] == '-') {
-            if(str[i+1] == '-' || str[i+1] == '+')
+        
+        if (str[i] == '+') {
+            if (str[i + 1] == '+' || str[i + 1] == '-') {
                 return 0;
-            else{
+            } else {
+                i++;
+            }
+        }
+        
+        if (str[i] == '-') {
+            if (str[i + 1] == '+' || str[i + 1] == '-') {
+                return 0;
+            } else {
                 sign = -1;
                 i++;
             }
         }
         
-        for(int j = i; j< length; j++){
-            if(str[j] == ' ') break;
-            if(str[j] <'0' || str[j] >'9')
+        for (int j = i; j < length; j++) {
+            if (str[j] == ' ') {
                 break;
-            if(INT_MAX/10<result || INT_MAX/10==result && INT_MAX%10<(str[j]-'0')){
-                return sign == -1? INT_MIN: INT_MAX;
+            } 
+            
+            if (str[j] < '0' || str[j] > '9') {
+                break;
             }
             
-            result = result*10 + (str[j]-'0');
+            if (INT_MAX / 10 < result || (INT_MAX / 10 == result && (str[j] - '0') > INT_MAX % 10 )) {
+                return sign == -1 ? INT_MIN : INT_MAX;
+            }
+            
+            result = result * 10 + (str[j] - '0');
         }
         
-        return result*sign;
+        return sign * result;
     }
 };
 
