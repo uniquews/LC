29f920804b7e83a49d8773a08ab2b8e3a0fc419c
uniquews <wstjpu@gmail.com>
2014-09-18 16:07:14 -0700

    update

--- SetMatrixZero/SetMatrixZero/main.cpp
+++ SetMatrixZero/SetMatrixZero/main.cpp
@@ -7,16 +7,72 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-    static void setZeros(vector<vector<int>>){
-    
-        size_t 
-    
+    void setZeroes(vector<vector<int> > &matrix) {
+        int row = matrix.size();
+        int column = matrix[0].size();
+        
+        bool firstRow = false;
+        bool firstColumn = false;
+        
+        for (int i = 0; i < column; i++) {
+            if (matrix[0][i] == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+        
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                firstColumn = true;
+                break;
+            }
+        }
+        
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < column; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 1; j < row; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < column; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if (firstRow == true) {
+            for (int i = 0; i < column; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        
+        if (firstColumn == true) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
+        return;
     }
-
+    
 };
 
 
