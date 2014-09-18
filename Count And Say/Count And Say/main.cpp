514f7cf6c969207dc03f65b6fa04321bfe416c0c
uniquews <wstjpu@gmail.com>
2014-09-17 20:15:11 -0700

    update

--- Count And Say/Count And Say/main.cpp
+++ Count And Say/Count And Say/main.cpp
@@ -7,12 +7,54 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string begin = "1";
+        
+        if (n == 0) {
+            return "";
+        }
+        
+        if (n == 1) {
+            return begin;
+        }
+        
+        while (n > 1) {
+            string newStr = "";
+            int i = 0;
+            while (i < begin.size()) {
+                char tmp = begin[i];
+                int j = i;
+                int start = i;
+                while (j < begin.size() && begin[j] == tmp) {
+                    j++;
+                }
+                
+                newStr.append(to_string(j - start));
+                newStr += tmp;
+                i = j;
+            
+            }
+            begin = newStr;
+            n--;
+            
+        }
+        
+      return begin;
+    }
+};
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n = 4;
+    Solution su;
+    cout << su.countAndSay(n) << endl;
     return 0;
 }
 
