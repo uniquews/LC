d8a0064716cce7a74e7c0703761deeba74210ec4
uniquews <wstjpu@gmail.com>
2014-09-17 22:20:57 -0700

    update

--- Length_Of_Last_Word/Length_Of_Last_Word/main.cpp
+++ Length_Of_Last_Word/Length_Of_Last_Word/main.cpp
@@ -10,30 +10,55 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int lengthOfLastWord(const char *s) {
+//        int count =0;
+//
+//        for(int i=(int)strlen(s)-1; i>=0; i--){
+//            if(::isalnum(s[i])){
+//                count++;
+//            }else{
+//                if(count == 0)
+//                    continue;
+//                else
+//                    return count;
+//            }
+//        }
+//        
+//        return count;
+//    }
+//};
+
 class Solution {
+
 public:
     int lengthOfLastWord(const char *s) {
-        int count =0;
-
-        for(int i=(int)strlen(s)-1; i>=0; i--){
-            if(::isalnum(s[i])){
-                count++;
-            }else{
-                if(count == 0)
-                    continue;
-                else
-                    return count;
+        int len = 0;
+        int result = 0;
+        while (*s != '\0') {
+            if (*s != ' ') {
+                len++;
+                result = len;
+                s++;
+            } else {
+                s++;
+                len = 0;
             }
+            
         }
         
-        return count;
+        return result;
+        
     }
+
+
 };
 
 int main(int argc, const char * argv[])
 {
 
-    char *s = "b a";
+    char *s = "a ";
     Solution su;
     cout << su.lengthOfLastWord(s) << endl;
     return 0;
--- Search_In_Rotated_Array/Search_In_Rotated_Array/main.cpp
+++ Search_In_Rotated_Array/Search_In_Rotated_Array/main.cpp
@@ -12,38 +12,76 @@
 using namespace std;
 
 
+//class Solution {
+//public:
+//    int search(int A[], int n, int target) {
+//        int first = 0;
+//        int last = n-1;
+//        
+//        while(first <=last){
+//            int mid = (first + last) /2;
+//            int midNum = A[mid];
+//            if(midNum == target)
+//                return mid;
+//            if(A[first] <= A[mid]){
+//                if(A[first] <= target && target < A[mid]){
+//                    last = mid-1;
+//                    
+//                }else{
+//                    first = mid +1;
+//                    
+//                }
+//                
+//            }else{
+//                if(A[mid]<target && target <= A[last]){
+//                    first = mid+1;
+//                }else{
+//                    last = mid-1;
+//                }
+//                
+//            }
+//            
+//        }
+//        
+//        return -1;
+//    }
+//};
+
 class Solution {
 public:
     int search(int A[], int n, int target) {
-        int first = 0;
-        int last = n-1;
+		int start = 0;
+		int end = n - 1;
         
-        while(first <=last){
-            int mid = (first + last) /2;
-            int midNum = A[mid];
-            if(midNum == target)
-                return mid;
-            if(A[first] <= A[mid]){
-                if(A[first] <= target && target < A[mid]){
-                    last = mid-1;
-                    
-                }else{
-                    first = mid +1;
-                    
-                }
-                
-            }else{
-                if(A[mid]<target && target <= A[last]){
-                    first = mid+1;
-                }else{
-                    last = mid-1;
-                }
-                
-            }
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if (A[mid] == target) {
+				return mid;
+			} else if (A[start] < A[mid]) {
+			    if (A[start] <= target && target < A[mid]) {
+			        end = mid;
+			    } else {
+			        start = mid;
+			    }
+			} else {
+			    if (A[mid] < target && target <= A[end]) {
+			        start = mid;
+			    } else {
+			        end = mid;
+			    }
+			}
             
-        }
+		}
         
-        return -1;
+		if (A[start] == target) {
+			return start;
+		}
+        
+		if (A[end] == target) {
+			return end;
+		}
+        
+		return -1;
     }
 };
 
@@ -51,8 +89,11 @@ public:
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int A[] = {4,5,6,7,8,1,2,3};
+    int target = 8;
+    Solution su;
+    cout << su.search(A, 8, 8) << endl;
+    
     return 0;
 }
 
