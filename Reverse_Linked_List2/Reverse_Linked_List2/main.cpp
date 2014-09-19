77670ef91a6fd627bb58e72d032841baa357824c
uniquews <wstjpu@gmail.com>
2014-09-18 18:02:34 -0700

    update

--- Reverse_Linked_List2/Reverse_Linked_List2/main.cpp
+++ Reverse_Linked_List2/Reverse_Linked_List2/main.cpp
@@ -1,3 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,29 +21,46 @@ class Solution {
 public:
     ListNode *reverseBetween(ListNode *head, int m, int n) {
         ListNode dummy(-1);
-        dummy.next = head;
-        
-        ListNode *prepareToReverse;
         ListNode *prev = &dummy;
+        dummy.next = head;
+        ListNode *prepare = nullptr;
         
-        for(int i=1; i<=n; i++){
-            if(m == i){
-                prepareToReverse = prev;
-            }
-            
-            if(i >m){
-                prev->next = head->next; //1
-                head->next = prepareToReverse->next;//2
-                prepareToReverse ->next = head;//2
-                head = prev;//1
+        for (int i = 1; i <= n; i++) {
+            if (m == i) {
+                prepare = prev;
+            } else if (i > m) {
+                prev->next = head->next;
+                head->next = prepare->next;
+                prepare->next = head;
+                head = prev;
             }
             
             prev = head;
             head = head->next;
-            
         }
         
-        
-        return dummy.next;
+        return dummy.next; // m=1时， prepare 指向dummy； m不等于1时，dummy.next本身也不会变
     }
+    
 };
+
+
+int main() {
+
+    ListNode * a = new ListNode(1);
+    ListNode * b = new ListNode(2);
+    ListNode * c = new ListNode(3);
+    ListNode * d = new ListNode(4);
+    
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = nullptr;
+    
+    Solution su;
+    cout << su.reverseBetween(a, 1, 4)->val << endl;
+    
+    
+    return 0;
+}
+
