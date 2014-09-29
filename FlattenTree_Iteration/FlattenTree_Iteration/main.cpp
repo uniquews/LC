b2dafb8369e72624906540eb707ebcf23af76a4a
uniquews <wstjpu@gmail.com>
2014-09-28 18:16:06 -0700

    update

--- FlattenTree_Iteration/FlattenTree_Iteration/main.cpp
+++ FlattenTree_Iteration/FlattenTree_Iteration/main.cpp
@@ -63,6 +63,8 @@ public:
 //    }
 //};
 
+
+
 int main(int argc, const char * argv[])
 {
 
--- Largest_Rectangle_In_Histogram/Largest_Rectangle_In_Histogram/main.cpp
+++ Largest_Rectangle_In_Histogram/Largest_Rectangle_In_Histogram/main.cpp
@@ -12,37 +12,77 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int largestRectangleArea(vector<int> &height) {
+//        if (height.size() == 0) {
+//            return 0;
+//        }
+//        
+//        int result = 0;
+//        stack<int> stk;
+//        for (int i = 0; i <= height.size(); i++) {
+//            int cur = i == height.size() ? -1 : height[i];
+//            while (stk.size() != 0 && cur < height[stk.top()]) {
+//                int h = height[stk.top()];
+//                stk.pop();
+//                
+//                int w = 0;
+//                if (stk.size() == 0) {
+//                    w = i; // 这个递增序列的前面有i个，递增序列的结尾是i - 1，被i弹出
+//                } else {
+//                    w = i - stk.top() - 1;
+//                }
+//                
+//                
+//                result = max(result, h * w);
+//                
+//            }
+//            
+//            stk.push(i);
+//        }
+//        
+//        return result;
+//    }
+//};
+
 class Solution {
 public:
     int largestRectangleArea(vector<int> &height) {
         if (height.size() == 0) {
-            return 0;
+            return  0;
         }
         
-        int result = 0;
-        stack<int> stk;
-        for (int i = 0; i <= height.size(); i++) {
-            int cur = i == height.size() ? -1 : height[i];
-            while (stk.size() != 0 && cur < height[stk.top()]) {
-                int h = height[stk.top()];
-                stk.pop();
-                
-                int w = 0;
-                if (stk.size() == 0) {
-                    w = i; // 这个递增序列的前面有i个，递增序列的结尾是i - 1，被i弹出
-                } else {
-                    w = i - stk.top() - 1;
-                }
-                
-                
-                result = max(result, h * w);
-                
+        int n = height.size();
+        int area = 0;
+        vector<int> left(height.size(), 0);
+        vector<int> right(height.size(), 0);
+        vector<int> result(height.size(), 0);
+        
+        left[0] = 0;
+        for (int i = 1; i < left.size(); i++) {
+            if (height[i] <= height[i - 1]) {
+                left[i] = left[i - 1];
+            } else {
+                left[i] = i;
             }
-            
-            stk.push(i);
         }
         
-        return result;
+        right[n - 1] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (height[i] > height[i + 1]) {
+                right[i] = i;
+            } else {
+                right[i] = right[i + 1];
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = (left[i] - right[i] + 1) * height[i];
+            area = max(result[i], area);
+        }
+        return area;
+        
     }
 };
 
@@ -54,7 +94,7 @@ int main(int argc, const char * argv[])
 //    vector<int> h = {100, 1000, 8};
     vector<int> h = {2,1,5,6,2,3};
     Solution su;
-    su.largestRectangleArea(h);
+    cout << su.largestRectangleArea(h) << endl;
     return 0;
 }
 
