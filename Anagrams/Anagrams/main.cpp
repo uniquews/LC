f483556392ca9cf595b968e2b15f7c7c304d95de
uniquews <wstjpu@gmail.com>
2014-10-03 17:54:35 -0700

    update

--- Anagrams/Anagrams/main.cpp
+++ Anagrams/Anagrams/main.cpp
@@ -13,22 +13,59 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    vector<string> anagrams(vector<string> &strs) {
+//        unordered_map<string, vector<string>> group;
+//        for(auto s: strs){
+//            string key = s;
+//            sort(key.begin(), key.end());
+//            group[key].push_back(s);
+//        }
+//        
+//        vector<string> result;
+//        for(auto it = group.begin(); it !=group.end(); it++){
+//            if(it->second.size() >1){
+//                result.insert(result.end(), it->second.begin(), it->second.end());
+//            }
+//        }
+//        
+//        return result;
+//    }
+//};
+
+
+
 class Solution {
 public:
     vector<string> anagrams(vector<string> &strs) {
-        unordered_map<string, vector<string>> group;
-        for(auto s: strs){
-            string key = s;
-            sort(key.begin(), key.end());
-            group[key].push_back(s);
+        if (strs.size() == 0) {
+            return vector<string> {};
         }
         
         vector<string> result;
-        for(auto it = group.begin(); it !=group.end(); it++){
-            if(it->second.size() >1){
+        
+        
+        unordered_map<string, vector<string>> _map;
+        for (int i = 0; i < strs.size(); i++) {
+            string tmp = strs[i];
+            sort(tmp.begin(), tmp.end());
+            if (_map.find(tmp) != _map.end()) {
+                _map[tmp].push_back(strs[i]);
+            } else {
+                _map[tmp].push_back(strs[i]);
+            }
+            
+        }
+        
+        
+        unordered_map<string, vector<string>> :: iterator it = _map.begin();
+        for (; it != _map.end(); it++) {
+            if (it->second.size() > 1) {
                 result.insert(result.end(), it->second.begin(), it->second.end());
             }
         }
+        
         
         return result;
     }
@@ -37,8 +74,9 @@ public:
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<string> strs = {"", ""};
+    Solution su;
+    su.anagrams(strs);
     return 0;
 }
 
