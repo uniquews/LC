d72e1ea660c37afbe70fb4826a02292f4dcb3b76
uniquews <wstjpu@gmail.com>
2014-10-07 16:04:53 -0700

    update

--- Maximum_Subarray2/Maximum_Subarray2/main.cpp
+++ Maximum_Subarray2/Maximum_Subarray2/main.cpp
@@ -11,98 +11,89 @@
 
 using namespace std;
 
+// Sum数组做法
 //class Solution {
+//    
 //public:
-//    int maxSubArray(vector<int> A) {
-//        if (A.size() == 0) {
+//    int maxTwoSubArrays(vector<int> &nums) {
+//        if (nums.size() == 0) {
 //            return 0;
 //        }
 //        
 //        int result = INT_MIN;
-//        int prevSum = 0;
-//        vector<int> sum(A.size(), 0);
-//        for (int i = 0; i < A.size(); i++) {
-//            sum[i] = prevSum + A[i];
-//            prevSum = sum[i];
+//        int n = (int)nums.size();
+//        vector<int> maxLeft(n + 1, INT_MIN);
+//        vector<int> maxRight(n + 1, INT_MIN);
+//        vector<int> profit(n + 1, 0);
+//        
+//        vector<int> sum(n + 1, 0);
+//        
+//        for (int i = 1; i <= n; i++) {
+//            sum[i] = sum[i - 1] + nums[i - 1];
 //        }
 //        
-//        vector<int> maxLeft(A.size());
-//        int valley = 0;
-//        maxLeft[0] = sum[0];
-//        for (int i = 1; i < A.size(); i++) {
-//            maxLeft[i] = max(sum[i] - valley, maxLeft[i - 1]);
-//            valley = min (sum[i], valley);
+//        maxLeft[0] = INT_MIN; //有可能第一个数是负数，所以不能初始化为0
+//        for (int i = 1, valley = 0; i < sum.size(); i++) {            
+//            maxLeft[i] = max(maxLeft[i - 1], sum[i] - valley);
+//            valley = min(valley, sum[i]);
 //        }
 //        
-//        vector<int> maxRight(A.size());
-//        int peek = sum[A.size() - 1];
-//        maxRight[A.size() - 1] = 0;
 //        
-//        for (int i = (int)A.size() - 2; i >= 0; i--) {
-//            maxRight[i] = max(maxRight[i + 1], peek - sum[i]);
-//            peek = max(peek, sum[i]);
-//        }
-//        
-//        for (int i = 0; i < A.size() - 1; i++) {
-//            
-//            if (maxLeft[i] + maxRight[i + 1] > result) {
-//                result = maxLeft[i] + maxRight[i];
+//        for (int i = n, peak = sum[n]; i > 0; i--) {
+//            if (i == n) {
+//                maxRight[i] = peak - sum[i - 1];  //因为是从n开始，由于n有含义（不像0无含义）。所以这个值其实就是原数组本身的最后一个数字他自己
+//            } else {
+//                maxRight[i]= max(maxRight[i + 1], peak - sum[i - 1]);
 //            }
+//            peak = max(peak, sum[i - 1]);  //由于[a,...b]的和是从[a,...b]里取一个peak减去a-1那个位置的sum,所以peak的范围是从sum[i - 1]
+//        }
+//        
+//        for (int i = 1; i < n; i++) {
+//            profit[i] = maxLeft[i] + maxRight[i + 1];
+//            result = max(result, profit[i]);
 //        }
 //        
 //        return result;
-//        
-//        
 //    }
-//
-//
+//    
 //};
 
+
+//普通dp做法
+
 class Solution {
-    
 public:
     int maxTwoSubArrays(vector<int> &nums) {
         if (nums.size() == 0) {
             return 0;
         }
         
-        int result = INT_MIN;
         int n = (int)nums.size();
-        vector<int> maxLeft(n + 1, INT_MIN);
-        vector<int> maxRight(n + 1, INT_MIN);
-        vector<int> profit(n + 1, 0);
-        
-        vector<int> sum(n + 1, 0);
+        vector<int> left(n + 1, 0);
+        vector<int> right(n + 1, 0);
+        vector<int> total(n + 1, 0);
         
         for (int i = 1; i <= n; i++) {
-            sum[i] = sum[i - 1] + nums[i - 1];
+            left[i] = max(left[i - 1] + nums[i - 1], nums[i - 1]);
+            left[i] = max(left[i - 1], left[i]);
         }
         
-        maxLeft[0] = INT_MIN;
-        for (int i = 1, valley = 0; i < sum.size(); i++) {            
-            maxLeft[i] = max(maxLeft[i - 1], sum[i] - valley);
-            valley = min(valley, sum[i]);
+        right[n] = nums[n - 1];
+        for (int i = n - 1; i > 0; i--) {
+            right[i] = max(right[i + 1] + nums[i - 1], nums[i - 1]);
+            right[i] = max(right[i + 1], right[i]);
         }
         
-        
-        for (int i = n, peak = sum[n]; i > 0; i--) {
-            if (i == n) {
-                maxRight[i] = peak - sum[i - 1];
-            } else {
-                maxRight[i]= max(maxRight[i + 1], peak - sum[i - 1]);
-            }
-            peak = max(peak, sum[i - 1]);
-        }
-        
+        int result = INT_MIN;
         for (int i = 1; i < n; i++) {
-            profit[i] = maxLeft[i] + maxRight[i + 1];
-            result = max(result, profit[i]);
+            total[i] = left[i] + right[i + 1];
+            result = max(total[i], result);
         }
-        
         return result;
     }
     
 };
+
 
 int main(int argc, const char * argv[])
 {
