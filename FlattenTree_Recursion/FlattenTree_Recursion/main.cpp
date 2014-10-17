56c80f354f151f7fece7cd35c7d175f52c3b7fd0
uniquews <wstjpu@gmail.com>
2014-10-16 21:32:33 -0700

    upadte

--- FlattenTree_Recursion/FlattenTree_Recursion/main.cpp
+++ FlattenTree_Recursion/FlattenTree_Recursion/main.cpp
@@ -17,88 +17,37 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-
-
-class Tree{
-public:
-    TreeNode *root;
-    Tree(TreeNode *t): root(t){};
-    
-};
-
-
-//class Solution {
-//public:
-//    void flatten(TreeNode *root) {
-//        if(root==nullptr)
-//            return;
-//        
-//        //if(root->left==nullptr) return;  // cannot put this here
-//        
-//        flatten(root->left);
-//        flatten(root->right);
-//        
-//        if(root->left==nullptr) return;// you must put this here after flatten the left and right sub tree.
-//        // the reason is that: is we put root->left == nullptr ahead, when input is  {1,#,2,3}, it will irectly return and not won't check the root->right sub tree.
-//        
-//        
-//        TreeNode *p = root->left;
-//        while(p->right) p = p->right;
-//        
-//        p->right = root->right;
-//        root->right = root->left;
-//        root->left = nullptr;
-//        
-//        return;
-//    }
-//};
-
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
     void flatten(TreeNode *root) {
         if (root == nullptr) {
             return;
         }
-        dfs(root);
-        return;
-    }
-    
-    TreeNode *dfs(TreeNode *node) {
-        if (node == nullptr) {
-            return nullptr;
-        }
-        if (node->left == nullptr && node->right == nullptr) {
-            return node;
-        }
         
-        TreeNode *leftNode = dfs(node->left);
+        TreeNode *leftNode = root->left;
+        TreeNode *rightNode = root->right;
         
-        TreeNode *rightNode = dfs(node->right);
+        flatten(leftNode);
+        flatten(rightNode);
+        
+        if (leftNode == nullptr && rightNode == nullptr) {
+            return;
+        }
+        root->left = nullptr;
+        
         TreeNode *tmp = leftNode;
-        while (tmp != nullptr && tmp->right != nullptr) {
+        if (tmp == nullptr) {
+            return;
+        }
+        
+        while (tmp->right != nullptr) {
             tmp = tmp->right;
         }
         
-        if (tmp != nullptr) {
-            tmp->right = rightNode;
-            node->right = leftNode;
-        }
-        
-        node->left = nullptr;
-        
-        return node;
-        
+        tmp->right = rightNode;
+        root->right = leftNode;
+        return;
     }
-    
 };
 
 
