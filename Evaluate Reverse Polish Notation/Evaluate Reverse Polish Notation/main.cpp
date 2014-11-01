9f67494f172ba67c824a2c10434eb4c0c89bf0be
uniquews <wstjpu@gmail.com>
2014-11-01 16:59:30 -0700

    update

--- Evaluate Reverse Polish Notation/Evaluate Reverse Polish Notation/main.cpp
+++ Evaluate Reverse Polish Notation/Evaluate Reverse Polish Notation/main.cpp
@@ -8,51 +8,49 @@
 
 #include <iostream>
 #include <vector>
-
 #include <string>
-
 #include <stack>
 using namespace std;
+
 
 class Solution {
 public:
     int evalRPN(vector<string> &tokens) {
+        int result = 0;
         stack<int> stk;
         for (int i = 0; i < tokens.size(); i++) {
-            if ((tokens[i][0] >= '0' && tokens[i][0] <= '9') ||
-                (tokens[i][0] == '-' && tokens[i].size() > 1)) {
+            if ((tokens[i][0] >= '0' && tokens[i][0] <= '9') || (tokens[i][0] == '-' && tokens[i][1] >= '0' && tokens[i][1] <= '9')) {
                 stk.push(stoi(tokens[i]));
             } else {
                 int num1 = stk.top();
                 stk.pop();
-                if (stk.size() == 0) {
-                    return num1;
-                }
                 int num2 = stk.top();
                 stk.pop();
-                int result = 0;
+                int tmp = 0;
                 if (tokens[i] == "+") {
-                    result = num1 + num2;
+                    tmp = num1 + num2;
                 } else if (tokens[i] == "-") {
-                    result = num2 - num1;
+                    tmp = num2 - num1;
                 } else if (tokens[i] == "*") {
-                    result = num2 * num1;
+                    tmp = num2 * num1;
                 } else {
-                    result = num2 / num1;
+                    tmp = num2 / num1;
                 }
                 
-                stk.push(result);
+                stk.push(tmp);
             }
         }
-        
-        return stk.top();
+        result = stk.top();
+        stk.pop();
+        return result;
     }
 };
 
 int main(int argc, const char * argv[])
 {
 
-    vector<string> tokens = {"-1","1","*","-1","+"};
+//    vector<string> tokens = {"-1","1","*","-1","+"};
+    vector<string> tokens = {"0", "3", "/"};
     Solution su;
     cout << su.evalRPN(tokens) << endl;
     return 0;
