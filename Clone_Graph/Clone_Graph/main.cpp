711613a4d5f8a3532ebbd5a2cc8a9940122e8540
uniquews <wstjpu@gmail.com>
2014-11-16 16:00:16 -0800

    update

--- Clone_Graph/Clone_Graph/main.cpp
+++ Clone_Graph/Clone_Graph/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
 
@@ -18,44 +19,101 @@ struct UndirectedGraphNode {
     UndirectedGraphNode(int x) : label(x) {};
 };
 
+//class Solution {
+//public:
+//    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+//        if (node == nullptr) {
+//            return nullptr;
+//        }
+//        
+//        unordered_map<UndirectedGraphNode *, UndirectedGraphNode *> hashMap;
+//        vector<UndirectedGraphNode *> nodeList;
+//        UndirectedGraphNode *newNode = new UndirectedGraphNode(node->label);
+//        hashMap[node] = newNode;
+//        nodeList.push_back(node);
+//        
+//        int start = 0;
+//        while (start < nodeList.size()) {
+//            UndirectedGraphNode *cur = nodeList[start];
+//            for (UndirectedGraphNode* n : cur->neighbors) {
+//                if (find(nodeList.begin(), nodeList.end(), n)  == nodeList.end()) {
+//                    UndirectedGraphNode *newNeigh = new UndirectedGraphNode(n->label);
+//                    hashMap[n] = newNeigh;
+//                    nodeList.push_back(n);
+//                }
+//            }
+//            
+//            start++;
+//        }
+//        
+//        
+//        for (int i = 0; i < nodeList.size(); i++) {
+//            UndirectedGraphNode *cur = nodeList[i];
+//            for (int j = 0; j < cur->neighbors.size(); j++) {
+//                UndirectedGraphNode *newCur = hashMap[cur];
+//                newCur->neighbors.push_back(hashMap[cur->neighbors[j]]);
+//            }
+//            
+//        }
+//        return hashMap[node];
+//        
+//    }
+//};
+
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
 class Solution {
 public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
         if (node == nullptr) {
-            return nullptr;
+            return node;
         }
         
-        unordered_map<UndirectedGraphNode *, UndirectedGraphNode *> hashMap;
         vector<UndirectedGraphNode *> nodeList;
-        UndirectedGraphNode *newNode = new UndirectedGraphNode(node->label);
-        hashMap[node] = newNode;
+        queue<UndirectedGraphNode *> q;
+        unordered_map<UndirectedGraphNode*, UndirectedGraphNode *> hashMap;
         nodeList.push_back(node);
+        UndirectedGraphNode *newStart = new UndirectedGraphNode(node->label);
+        hashMap[node] = newStart;
+        q.push(node);
+        int curLevel = (int)q.size();
+        int nextLevel = 0;
         
-        int start = 0;
-        while (start < nodeList.size()) {
-            UndirectedGraphNode *cur = nodeList[start];
-            for (UndirectedGraphNode* n : cur->neighbors) {
-                if (find(nodeList.begin(), nodeList.end(), n)  == nodeList.end()) {
-                    UndirectedGraphNode *newNeigh = new UndirectedGraphNode(n->label);
-                    hashMap[n] = newNeigh;
-                    nodeList.push_back(n);
+        while (curLevel != 0) {
+            UndirectedGraphNode *cur = q.front();
+            q.pop();
+            curLevel--;
+            for (int i = 0; i < cur->neighbors.size(); i++) {
+                if (hashMap.find(cur->neighbors[i]) == hashMap.end()) {
+                    UndirectedGraphNode *newCurNeighbor = new UndirectedGraphNode(cur->neighbors[i]->label);
+                    q.push(cur->neighbors[i]);
+                    hashMap[cur->neighbors[i]] = newCurNeighbor;
+                    nodeList.push_back(cur->neighbors[i]);
+                    nextLevel++;
+                    
                 }
             }
             
-            start++;
+            if (curLevel == 0) {
+                curLevel = nextLevel;
+                nextLevel = 0;
+            }
         }
-        
         
         for (int i = 0; i < nodeList.size(); i++) {
             UndirectedGraphNode *cur = nodeList[i];
             for (int j = 0; j < cur->neighbors.size(); j++) {
-                UndirectedGraphNode *newCur = hashMap[cur];
-                newCur->neighbors.push_back(hashMap[cur->neighbors[j]]);
+                hashMap[cur]->neighbors.push_back(hashMap[cur->neighbors[j]]);
             }
-            
         }
-        return hashMap[node];
         
+        return newStart;
     }
 };
 
@@ -65,14 +123,15 @@ int main(int argc, const char * argv[])
 
     UndirectedGraphNode *a = new UndirectedGraphNode(0);
     UndirectedGraphNode *b = new UndirectedGraphNode(1);
-    UndirectedGraphNode *c = new UndirectedGraphNode(1);
+    UndirectedGraphNode *c = new UndirectedGraphNode(2);
     
     a->neighbors.push_back(b);
     b->neighbors.push_back(c);
     c->neighbors.push_back(c);
     
     Solution su;
-    cout << su.cloneGraph(a) -> label << endl;
+    UndirectedGraphNode *result = su.cloneGraph(a);
+    cout << result -> label << endl;
     
     return 0;
 }
