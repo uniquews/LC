699eece69d5cbc17f9e1519b3aa62fe19eff8eaa
uniquews <wstjpu@gmail.com>
2014-11-16 16:31:08 -0800

    update

--- Palindrome_Partitioning2/Palindrome_Partitioning2/main.cpp
+++ Palindrome_Partitioning2/Palindrome_Partitioning2/main.cpp
@@ -11,15 +11,16 @@
 #include <vector>
 
 using namespace std;
+
 class Solution {
 public:
     int minCut(string s) {
-        if (s.size() == 0) {
+        if (s.size() == 0 || s.size() == 1) {
             return 0;
         }
         
         vector<vector<bool>> isPP(s.size(), vector<bool>(s.size(), false));
-        
+        vector<int> f(s.size() + 1, INT_MAX);
         for (int i = (int)s.size() - 1; i >= 0; i--) {
             for (int j = i; j < s.size(); j++) {
                 if (s[i] == s[j] && (j - i <= 2 || isPP[i + 1][j - 1])) {
@@ -28,26 +29,23 @@ public:
             }
         }
         
-        vector<int> f(s.size() + 1, INT_MAX);
-        f[0] = 0;
-        
+        f[0] = -1;
         for (int i = 1; i <= s.size(); i++) {
             for (int j = 0; j < i; j++) {
                 if (isPP[j][i - 1]) {
-                    f[i] = min(f[i], f[j] + 1);
+                    f[i] = min(f[j] + 1, f[i]);
                 }
             }
         }
         
         return f[s.size()];
-        
     }
 };
 
 int main(int argc, const char * argv[])
 {
 
-    string s = "ccd";
+    string s = "ccc";
     Solution su;
     cout << su.minCut(s) << endl;
     return 0;
